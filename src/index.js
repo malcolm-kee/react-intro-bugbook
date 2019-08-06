c9e5815d555502b34aff5af4b856fbf0373d0f12
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-08-06 09:19:44 +0800

    Day 1

--- src/index.js
+++ src/index.js
@@ -1,77 +1,111 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-const postData = [
-  {
-    id: 1111,
-    author: {
-      src: 'https://twitter.com/dog_feelings/status/1130660155966730240',
-      name: 'Thoughts of Dog 🏳️‍🌈',
-    },
-    post: {
-      text: 'to be perfectly clear. i love you',
-      image: null,
-    },
-  },
-  {
-    id: 1,
-    author: {
-      src: 'https://twitter.com/markdalgleish/status/1095424850565378048',
-      name: 'Mark Dalgleish',
-    },
-    post: {
-      text:
-        "If you're not mocking out Lodash, can you really call it a unit test?",
-      image: null,
-    },
-  },
-  {
-    id: 2,
-    author: {
-      src: 'https://twitter.com/iamdevloper/status/1081923027644882944',
-      name: 'I Am Devloper',
-    },
-    post: {
-      text:
-        'when you visit a site you handed over 6 months ago and the client made some changes themselves',
-      image: {
-        src: 'https://bugbook.netlify.com/images/picture-after-client.jpg',
-        alt: 'Ugly photo',
-      },
-    },
-  },
-];
+function LikeButton(props) {
+  return (
+    <button
+      onClick={props.onClick}
+      type="button"
+      className={props.isLiked ? 'button button-liked' : 'button'}
+    >
+      Like
+    </button>
+  );
+}
+
+function LikeAllButton(props) {
+  return (
+    <div className="card ads-card">
+      <div className="card-small-title">Exclusive Features for You!</div>
+      <button
+        onClick={props.onClick}
+        id="like-all-btn"
+        className="btn"
+        type="button"
+      >
+        Like All Posts
+      </button>
+    </div>
+  );
+}
+
+function Post(props) {
+  const post = props.data;
+  return (
+    <article className="card post" key={post.id}>
+      <div className="card-title">
+        <a href={post.author.src}>{post.author.name}</a>
+        <button className="focus-btn" type="button">
+          ...
+        </button>
+      </div>
+      <div className="card-content">{post.post.text}</div>
+      {post.post.image && (
+        <div className="card-image-container">
+          <img
+            className="card-image"
+            src={post.post.image.src}
+            alt={post.post.image.alt}
+          />
+        </div>
+      )}
+      <div className="card-actions">
+        <LikeButton isLiked={props.liked} onClick={props.onLikeClick} />
+      </div>
+    </article>
+  );
+}
 
 function Feed(props) {
+  const likedIds = props.likedIds;
   return (
     <div className="feed">
       {props.posts.map(post => (
-        <article className="card post" key={post.id}>
-          <div className="card-title">
-            <a href={post.author.src}>{post.author.name}</a>
-            <button className="focus-btn" type="button">
-              ...
-            </button>
-          </div>
-          <div className="card-content">{post.post.text}</div>
-          {post.post.image && (
-            <div className="card-image-container">
-              <img
-                className="card-image"
-                src={post.post.image.src}
-                alt={post.post.image.alt}
-              />
-            </div>
-          )}
-          <div className="card-actions">
-            <button type="button" className="button">
-              Like
-            </button>
-          </div>
-        </article>
+        <Post
+          liked={likedIds.includes(post.id)}
+          onLikeClick={() => props.toggleLike(post.id)}
+          data={post}
+        />
       ))}
     </div>
   );
 }
 
-ReactDOM.render(<Feed posts={postData} />, document.getElementById('root'));
+function App() {
+  const [likedIds, setLikeIds] = React.useState([]);
+  const [postData, setPostData] = React.useState([]);
+  const [page, setPage] = React.useState(1);
+
+  React.useEffect(() => {
+    axios
+      .get(`https://bugbook-server.herokuapp.com/posts?_page=${page}&_limit=3`)
+      .then(res => setPostData(postData.concat(res.data)));
+  }, [page]);
+
+  function toggleLike(id) {
+    if (likedIds.includes(id)) {
+      setLikeIds(likedIds.filter(i => i !== id));
+    } else {
+      setLikeIds(likedIds.concat(id));
+    }
+  }
+
+  return (
+    <div>
+      <Feed likedIds={likedIds} toggleLike={toggleLike} posts={postData} />
+      <button
+        onClick={() => {
+          setPage(page + 1);
+        }}
+      >
+        Load More
+      </button>
+      <LikeAllButton
+        onClick={() => setLikeIds(postData.map(post => post.id))}
+      />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
